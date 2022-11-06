33abfc2ca360495c970425489115bb910b81b6ef
Avaer Kazmer <a@modules.io>
2022-11-05 23:42:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ wget -O ./res101.pth https://cloudstor.aarnet.edu.au/plus/s/lTIJF4vrvHCAI31/down
 pip3 install ipykernel matplotlib opencv-python
 pip3 install torch==1.6.0 torchvision==0.7.0
 #cudatoolkit==10.2
+pip3 install flask
