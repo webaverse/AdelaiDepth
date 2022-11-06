524cb83c402e2ba25c3c1e1622214b1bc50b8ff9
Avaer Kazmer <a@modules.io>
2022-11-05 23:10:52 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+wget -O ./res101.pth https://cloudstor.aarnet.edu.au/plus/s/lTIJF4vrvHCAI31/download
+
+pip3 install ipykernel matplotlib opencv-python
+pip3 install torch==1.6.0 torchvision==0.7.0
+#cudatoolkit==10.2
