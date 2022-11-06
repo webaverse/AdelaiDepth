d7d0cd8dd217ad290f52fc57deb8d258d58a54e1
Avaer Kazmer <a@modules.io>
2022-11-06 16:02:46 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,4 +8,7 @@ conda activate adelai
 pip3 install -r requirements.txt
 
 sudo apt-get install -y libsparsehash-dev
-pip install --upgrade git+https://github.com/mit-han-lab/torchsparse.git@v1.2.0
+pip install --upgrade git+https://github.com/mit-han-lab/torchsparse.git@v1.0.0
+
+# conda install cudatoolkit=10.2
+sudo apt install -y nvidia-cuda-toolkit
