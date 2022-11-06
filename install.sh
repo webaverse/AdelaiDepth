d193633877e34f87455faf6a1810c2eefe5579e3
Avaer Kazmer <a@modules.io>
2022-11-06 00:00:29 -0400

    Add conda env to install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,7 @@
 #!/bin/bash
 
 wget -O ./res101.pth https://cloudstor.aarnet.edu.au/plus/s/lTIJF4vrvHCAI31/download
+
+conda create --name adelai
+conda activate adelai
 pip3 install -r requirements.txt
