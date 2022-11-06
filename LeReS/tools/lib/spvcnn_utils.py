69971bdb3ac12beb34774df822f063af02993962
Avaer Kazmer <a@modules.io>
2022-11-06 15:14:57 -0500

    Update spvcnn_utils.py

--- LeReS/tools/lib/spvcnn_utils.py
+++ LeReS/tools/lib/spvcnn_utils.py
@@ -1,5 +1,6 @@
 import torchsparse.nn.functional as spf
-from torchsparse.point_tensor import PointTensor
+# from torchsparse.point_tensor import PointTensor
+from torchsparse import PointTensor
 from torchsparse.utils.kernel_region import *
 from torchsparse.utils.helpers import *
 
