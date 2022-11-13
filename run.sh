6559fb9b4a290ccb68dd5abf13242dbdeb5b27d5
Avaer Kazmer <a@modules.io>
2022-11-12 19:38:05 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 conda activate adelai
 # sudo $(which python3) LeReS/tools/test_shape.py
-nohup sudo $(which python3) LeReS/tools/test_shape.py
+nohup sudo $(which python3) LeReS/tools/test_shape.py &
 tail -f /home/ubuntu/nohup.out
