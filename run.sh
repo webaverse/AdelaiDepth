97fdbc69e4eb99cb3dd02f8af3eb69a55d8b2a6d
listofbanned <listofbanned@gmail.com>
2023-01-17 10:15:34 -0300

    add nohup: err log

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 conda activate adelai
 # sudo $(which python3) LeReS/tools/test_shape.py
-nohup sudo $(which python3) LeReS/tools/test_shape.py &
-tail -f nohup.out
+# nohup sudo $(which python3) LeReS/tools/test_shape.py &
+# tail -f nohup.out
+nohup sudo $(which python3) app.py >out.log 2>out.err < /dev/null &
